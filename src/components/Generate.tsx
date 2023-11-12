c2e3a993daba2ca4c8c34ca74822bd3fd0f5715c
Manas Verma <manas.verma@gmail.com>
2023-11-12 13:16:18 -0600

    feat: making scramble input wider

--- src/components/Generate.tsx
+++ src/components/Generate.tsx
@@ -86,7 +86,7 @@ export function Generate() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center space-y-4 p-6 w-1/2">
+    <div className="flex flex-col items-center justify-center space-y-4 p-6 w-full">
       <div className="flex flex-col items-center justify-center w-full h-full border-white">
         Scramble
       </div>
